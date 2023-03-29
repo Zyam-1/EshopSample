21201222030c61cadd80adbdd6dd8b226826b493
Zyam-1 <zyammaqsood7@gmail.com>
2023-03-30 01:02:28 +0500

    Database and Seeders

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,8 @@
 <div class = "container w-25 p-3 mt-5">
 <form>
     <div class="mb-3">
+      <h3 style = "text-align: center">Login</h3>
+      <hr>
       <label for="exampleInputEmail1" class="form-label">Email address</label>
       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
       <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
